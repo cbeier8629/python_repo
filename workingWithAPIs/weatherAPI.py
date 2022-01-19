70e1350d3224a25fe306732800e7b05988c32c48
Chris Beier <chris.beier@go2si.com>
2022-01-19 08:01:03 -0500

    removed api key

--- workingWithAPIs/weatherAPI.py
+++ workingWithAPIs/weatherAPI.py
@@ -3,6 +3,6 @@
 import requests
 
 response = requests.get(
-    "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=39c9862ba1a1e9732d3f0380de1d4892")
+    "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid={api}")
 
 print(response.status_code)
